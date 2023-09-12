5fd028398f3648b3755e01a20988a3d3fe414e98
Elliot Mitchell <emitchell@microsoft.com>
2023-09-12 12:29:53 -0400

    Chapter 4

--- app/src/main/java/com/opengl/playground/PlaygroundRenderer.kt
+++ app/src/main/java/com/opengl/playground/PlaygroundRenderer.kt
@@ -3,13 +3,14 @@ package com.opengl.playground
 import android.content.Context
 import android.opengl.GLES20.GL_COLOR_BUFFER_BIT
 import android.opengl.GLES20.GL_POINTS
-import android.opengl.GLES20.GL_TRIANGLES
+import android.opengl.GLES20.GL_TRIANGLE_FAN
 import android.opengl.GLES20.glClear
 import android.opengl.GLES20.glClearColor
 import android.opengl.GLES20.glDrawArrays
 import android.opengl.GLES20.glEnableVertexAttribArray
 import android.opengl.GLES20.glGetAttribLocation
 import android.opengl.GLES20.glGetUniformLocation
+import android.opengl.GLES20.glLineWidth
 import android.opengl.GLES20.glUniform4f
 import android.opengl.GLES20.glUseProgram
 import android.opengl.GLES20.glVertexAttribPointer
@@ -26,36 +27,45 @@ import javax.microedition.khronos.opengles.GL10
 
 class PlaygroundRenderer(val context: Context) : Renderer {
     companion object {
-        private const val U_COLOR = "u_Color"
-        private const val A_POSITION = "a_Position"
-
         // x,y coordinate components
-        private const val POSITION_COMPONENT_COUNT = 2
         private const val BYTES_PER_FLOAT = 4
+        // 2 position components
+        private const val POSITION_COMPONENT_COUNT = 2
+        // 3 color components
+        private const val COLOR_COMPONENT_COUNT = 3
+
+        private const val A_POSITION = "a_Position"
+        private const val A_COLOR = "a_Color"
+        private const val STRIDE =
+            (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT
     }
 
     private var program: Int = 0
-    private var uColorLocation: Int = 0
+    private var aColorLocation: Int = 0
     private var aPositionLocation: Int = 0
 
     private val tableVertices = floatArrayOf(
-        // TRIANGLE 1
-        -0.5f, -0.5f,
-        0.5f, 0.5f,
-        -0.5f, 0.5f,
+        // Order of coordinates: X, Y, R, G, B
 
-        // TRIANGLE 2
-        -0.5f, -0.5f,
-        0.5f, -0.5f,
-        0.5f, 0.5f,
+        // Triangle Fan
+        0f, 0f, 1f, 1f, 1f, // center
+        -0.5f, -0.5f, 0.7f, 0.7f, 0.7f, // left bottom
+        0f, -0.5f, 0.7f, 0.7f, 0.7f, // bottom vertical
+        0.5f, -0.5f, 0.7f, 0.7f, 0.7f, // right bottom
+        0.5f, 0f, 0.7f, 0.7f, 0.7f, // right horizontal
+        0.5f, 0.5f, 0.7f, 0.7f, 0.7f, // right top
+        0f, 0.5f, 0.7f, 0.7f, 0.7f, // top vertical
+        -0.5f, 0.5f, 0.7f, 0.7f, 0.7f, // left top
+        -0.5f, 0f, 0.7f, 0.7f, 0.7f, // left horizontal
+        -0.5f, -0.5f, 0.7f, 0.7f, 0.7f, // left bottom
 
-        // LINE 1
-        -0.5f, 0f,
-        0.5f, 0f,
+        // Line 1
+        -0.5f, 0f, 1f, 0f, 0f, // left middle
+        0.5f, 0f, 0f, 1f, 1f, // right middle
 
         // Mallets
-        0f, -0.25f,
-        0f, 0.25f
+        0f, -0.25f, 0f, 0f, 1f, // blue mallet
+        0f, 0.25f, 1f, 0f, 0f // red mallet
 
     )
 
@@ -80,8 +90,8 @@ class PlaygroundRenderer(val context: Context) : Renderer {
         ShaderHelper.validateProgram(program)
         glUseProgram(program)
 
-        uColorLocation = glGetUniformLocation(program, U_COLOR).also {
-            log("uColorLocation: $it")
+        aColorLocation = glGetAttribLocation(program, A_COLOR).also {
+            log("aColorLocation: $it")
         }
 
         aPositionLocation = glGetAttribLocation(program, A_POSITION).also {
@@ -96,12 +106,25 @@ class PlaygroundRenderer(val context: Context) : Renderer {
             POSITION_COMPONENT_COUNT,
             GL10.GL_FLOAT,
             false,
-            0,
+            STRIDE,
             vertexData
         )
 
         // must enable the vertex attribute array
         glEnableVertexAttribArray(aPositionLocation)
+
+        // now updating position to point to the color data
+        vertexData.position(POSITION_COMPONENT_COUNT)
+
+        glVertexAttribPointer(
+            aColorLocation,
+            COLOR_COMPONENT_COUNT,
+            GL10.GL_FLOAT,
+            false,
+            STRIDE,
+            vertexData
+        )
+        glEnableVertexAttribArray(aColorLocation)
     }
 
     override fun onSurfaceChanged(p0: GL10?, width: Int, height: Int) {
@@ -115,28 +138,24 @@ class PlaygroundRenderer(val context: Context) : Renderer {
         glClear(GL_COLOR_BUFFER_BIT)
 
         // draw the WHITE table
-        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f)
-        // draw triangle
         glDrawArrays(
-            GL_TRIANGLES, // TRIANGLES
+            GL_TRIANGLE_FAN, // TRIANGLES
             0, // start at the beginning of the array
-            6 // draw 6 vertices, which is two triangles in our data
+            10 // draw 6 vertices, which is two triangles in our data
         )
 
         // draw the RED dividing line
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f)
         glDrawArrays(
             GL10.GL_LINES, // LINES
-            6, // start at index 6 (after the first 6 vertices)
+            10, // start at index 6 (after the first 6 vertices)
             2 // draw 2 vertices, which is one line in our data
         )
+        glLineWidth(10.0f)
 
         // draw the first BLUE mallet
-        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f)
-        glDrawArrays(GL_POINTS, 8, 1)//skip the first vertices, draw the next one
+        glDrawArrays(GL_POINTS, 12, 1)//skip the first vertices, draw the next one
 
         //draw the second RED MALLET
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f)
-        glDrawArrays(GL_POINTS, 9, 1)//skip the first vertices, draw the next one
+        glDrawArrays(GL_POINTS, 13, 1)//skip the first vertices, draw the next one
     }
 }
