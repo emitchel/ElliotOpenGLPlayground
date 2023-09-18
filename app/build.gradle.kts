c191a8a413fdd54b4aea89462ec1204a82ad3b3c
Elliot Mitchell <emitchell@microsoft.com>
2023-09-18 17:31:08 -0400

    Added some launch points for demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -54,6 +57,8 @@ dependencies {
     implementation("androidx.camera:camera-core:1.2.3")
     implementation("androidx.camera:camera-lifecycle:1.2.3")
     implementation("androidx.lifecycle:lifecycle-common:2.7.0-alpha02")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/opengl/LaunchingActivity.kt
@@ -0,0 +1,35 @@
+package com.opengl
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.opengl.camera.CameraActivity
+import com.opengl.playground.airhockey.AirHockeyActivity
+import com.opengl.playground.databinding.ActivityLaunchingBinding
+import com.opengl.playground.particles.ParticlesActivity
+
+class LaunchingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLaunchingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLaunchingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.icehockey.setOnClickListener {
+            // start CameraActivity
+            startActivity(Intent(this, AirHockeyActivity::class.java))
+        }
+
+        binding.duetCamera.setOnClickListener {
+            // start CameraActivity
+            startActivity(Intent(this, CameraActivity::class.java))
+        }
+
+        binding.particles.setOnClickListener {
+            // start CameraActivity
+            startActivity(Intent(this, ParticlesActivity::class.java))
+        }
+    }
+}
