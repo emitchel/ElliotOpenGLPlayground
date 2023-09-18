458e5dd6af82595f9cb0684e04f48fb4674fed9b
Elliot Mitchell <emitchell@microsoft.com>
2023-09-18 15:08:39 -0400

    Ok matching the mask and the camera

--- app/src/main/java/com/opengl/camera/programs/SegmentationOnlyCameraProgram.kt
+++ app/src/main/java/com/opengl/camera/programs/SegmentationOnlyCameraProgram.kt
@@ -29,7 +29,6 @@ import com.opengl.playground.programs.TextureShaderProgram
 import com.opengl.playground.util.TextureHelper
 import com.opengl.playground.util.log
 import java.nio.ByteBuffer
-import java.nio.ByteOrder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -71,39 +70,167 @@ class SegmentationOnlyCameraProgram(
         }
     }
 
-    fun FloatArray.toByteBuffer(): ByteBuffer {
-        val buffer = ByteBuffer.allocateDirect(size * 4) // 4 bytes per float
-        buffer.order(ByteOrder.nativeOrder()).asFloatBuffer().put(this)
-        buffer.position(0)  // Reset position
-        return buffer
+
+    private val lock = Any()
+    fun updateMaskData(byteBuffer: ByteBuffer, width: Int, height: Int) {
+
+        // synchronized(lock) {
+            this.byteBuffer = byteBuffer
+            this.byteBufferHeight = height
+            this.byteBufferWidth = width
+        // }
     }
 
-    fun segmentMaskCallback(it: SegmentationMask) {
-        coroutineScope.launch(Dispatchers.IO) {
-            val maskWidth = it.getWidth()
-            val maskHeight = it.getHeight()
+    override fun onSurfaceChanged(width: Int, height: Int) {
+        this.viewPortWidth = width
+        this.viewPortHeight = height
+        startCamera()
+    }
 
-            // The size of the buffer remains the same
+    private
+    val vertexArray: VertexArray = VertexArray(VERTEX_DATA)
 
-            val buffer = ByteBuffer.allocateDirect(maskWidth * maskHeight * 4) // 4 bytes for RGBA
+    private val modelMatrix = FloatArray(16)
 
-            for (i in 0 until maskWidth * maskHeight) {
-                val backgroundLikelihood = 1 - it.buffer.float
-                @ColorInt val color: Int
+    override fun onDrawFrame() {
 
-                if (backgroundLikelihood > 0.9) {
-                    color = Color.argb(128, 255, 0, 255)
-                } else if (backgroundLikelihood > 0.2) {
-                    val alpha = (182.9 * backgroundLikelihood - 36.6 + 0.5).toInt()
-                    color = Color.argb(alpha, 255, 0, 255)
-                } else {
-                    color = Color.TRANSPARENT
-                }
-                buffer.putInt(color)
-            }
-            buffer.flip()
-            updateMaskData(buffer, maskWidth, maskHeight)
+        // log("pulled latest mask, size: ${masks.size}")
+        // set positioning... i think
+        positionCameraCorrectly()
+        // setup the shaders to run
+        GLES20.glUseProgram(program)
+
+        surfaceTexture?.updateTexImage()
+        GLES20.glDisable(GLES20.GL_DEPTH_TEST);
+
+        // 1. Enable blending
+        GLES20.glEnable(GLES20.GL_BLEND);
+        // 2. Set the blend function
+        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+        // Bind the camera texture
+        // Pass the matrix into the shader program.
+        GLES20.glUniformMatrix4fv(getuMatrixLocation(), 1, false, modelMatrix, 0)
+        // Set the active texture unit to texture unit 0 for camera feed.
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, cameraTextureId);
+        GLES20.glUniform1i(
+            getuTextureUnitLocation(),
+            0
+        );
+
+        // bind the triangle + texture data
+        vertexArray.setVertexAttribPointer(
+            0,
+            getPositionAttributeLocation(),
+            POSITION_COMPONENT_COUNT,
+            STRIDE
+        )
+        vertexArray.setVertexAttribPointer(
+            POSITION_COMPONENT_COUNT,
+            getTextureCoordinatesAttributeLocation(),
+            TEXTURE_COORDINATES_COMPONENT_COUNT,
+            STRIDE
+        )
+        //Draw right away for the camera positioning
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
+
+        // Don't draw the segmentation mask if it hasn't been updated yet.
+        if (byteBuffer == null) {
+            return
         }
+        // synchronized(lock) {
+        if (maskTextureId == -1) {
+            maskTextureId =
+                TextureHelper.createTextureFromColors(
+                    byteBuffer!!,
+                    byteBufferWidth,
+                    byteBufferHeight
+                )
+        }
+
+        // positionSegmentationCorrectly()
+        // GLES20.glUniformMatrix4fv(getuMatrixLocation(), 1, false, modelMatrix, 0)
+
+        byteBuffer!!.flip()
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE1)
+        GLES20.glBindTexture(
+            GLES20.GL_TEXTURE_2D,
+            maskTextureId
+        )
+
+        GLES20.glTexSubImage2D(
+            GLES20.GL_TEXTURE_2D, 0, 0, 0,
+            byteBufferWidth, byteBufferHeight, GLES20.GL_RGBA,
+            GLES20.GL_UNSIGNED_BYTE, byteBuffer!!
+        )
+        // }
+        GLES20.glUniform1i(getuMaskTextureUnitLocation(), 1)
+
+        // bind the triangle + texture data
+        vertexArray.setVertexAttribPointer(
+            0,
+            getPositionAttributeLocation(),
+            POSITION_COMPONENT_COUNT,
+            STRIDE
+        )
+        vertexArray.setVertexAttribPointer(
+            POSITION_COMPONENT_COUNT,
+            getTextureCoordinatesAttributeLocation(),
+            TEXTURE_COORDINATES_COMPONENT_COUNT,
+            STRIDE
+        )
+
+        // lastly, draw the vertices...
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
+
+        GLES20.glDisable(GLES20.GL_BLEND);
+    }
+
+    private fun positionCameraCorrectly() {
+        Matrix.setIdentityM(modelMatrix, 0)
+        // As far as I can tell, the camera preview always comes in sideways.
+        // Every app who manages their own surface texture has to rotate it and set the scale.
+
+        val cameraAspectRatio = 1080f / 1920f   // 0.5625
+        val viewportAspectRatio = viewPortWidth.toFloat() / viewPortHeight.toFloat()
+        if (viewportAspectRatio > cameraAspectRatio) {
+            // The viewport is wider than the camera feed.
+            // We scale up in the Y direction to fill and crop.
+            val scale = viewportAspectRatio / cameraAspectRatio
+            Matrix.scaleM(modelMatrix, 0, 1f, scale, 1f)
+        } else {
+            // The viewport is taller than the camera feed.
+            // We scale up in the X direction to fill and crop.
+            val scale = cameraAspectRatio / viewportAspectRatio
+            Matrix.scaleM(modelMatrix, 0, scale, 1f, 1f)
+        }
+        // Matrix.rotateM(modelMatrix, 0, 90f, 0f, 0f, 1f)
+        // val mirrorMatrix = floatArrayOf(
+        //     -1f, 0f, 0f, 0f,
+        //     0f, 1f, 0f, 0f,
+        //     0f, 0f, 1f, 0f,
+        //     0f, 0f, 0f, 1f
+        // )
+        //
+        // Matrix.multiplyMM(modelMatrix, 0, mirrorMatrix, 0, modelMatrix, 0)
+
+        // TODO use this to scale and move the camera !!!
+        Matrix.scaleM(modelMatrix, 0, .5f, .5f, .5f)
+    }
+
+    companion object {
+        private const val POSITION_COMPONENT_COUNT = 2
+        private const val TEXTURE_COORDINATES_COMPONENT_COUNT = 2
+        private val STRIDE: Int = (POSITION_COMPONENT_COUNT
+            + TEXTURE_COORDINATES_COMPONENT_COUNT) * AirHockeyRenderer.BYTES_PER_FLOAT
+        private val VERTEX_DATA = floatArrayOf(
+            // Order of coordinates: X, Y, S, T
+            -1f, -1f, 0f, 1f,     // Bottom-left
+            1f, -1f, 1f, 1f,      // Bottom-right
+            1f, 1f, 1f, 0f,       // Top-right
+            -1f, 1f, 0f, 0f,      // Top-left
+            -1f, -1f, 0f, 1f      // Bottom-left again to close the fan
+        )
     }
 
     private fun startCamera() {
@@ -183,186 +310,31 @@ class SegmentationOnlyCameraProgram(
         }, ContextCompat.getMainExecutor(context))
     }
 
-    private val lock = Any()
-    fun updateMaskData(byteBuffer: ByteBuffer, width: Int, height: Int) {
+    fun segmentMaskCallback(it: SegmentationMask) {
+        coroutineScope.launch(Dispatchers.IO) {
+            val maskWidth = it.getWidth()
+            val maskHeight = it.getHeight()
 
-        // synchronized(lock) {
-            this.byteBuffer = byteBuffer
-            this.byteBufferHeight = height
-            this.byteBufferWidth = width
-        // }
-    }
+            // The size of the buffer remains the same
 
-    override fun onSurfaceChanged(width: Int, height: Int) {
-        this.viewPortWidth = width
-        this.viewPortHeight = height
-        startCamera()
-    }
+            val buffer = ByteBuffer.allocateDirect(maskWidth * maskHeight * 4) // 4 bytes for RGBA
 
-    private
-    val vertexArray: VertexArray = VertexArray(VERTEX_DATA)
+            for (i in 0 until maskWidth * maskHeight) {
+                val backgroundLikelihood = 1 - it.buffer.float
+                @ColorInt val color: Int
 
-    private val modelMatrix = FloatArray(16)
-
-    override fun onDrawFrame() {
-
-        // log("pulled latest mask, size: ${masks.size}")
-        // set positioning... i think
-        positionCameraCorrectly()
-        // setup the shaders to run
-        GLES20.glUseProgram(program)
-
-        surfaceTexture?.updateTexImage()
-        GLES20.glDisable(GLES20.GL_DEPTH_TEST);
-
-        // 1. Enable blending
-        GLES20.glEnable(GLES20.GL_BLEND);
-        // 2. Set the blend function
-        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
-        // Bind the camera texture
-        // Pass the matrix into the shader program.
-        GLES20.glUniformMatrix4fv(getuMatrixLocation(), 1, false, modelMatrix, 0)
-        // Set the active texture unit to texture unit 0 for camera feed.
-        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, cameraTextureId);
-        GLES20.glUniform1i(
-            getuTextureUnitLocation(),
-            0
-        );
-
-        // bind the triangle + texture data
-        vertexArray.setVertexAttribPointer(
-            0,
-            getPositionAttributeLocation(),
-            POSITION_COMPONENT_COUNT,
-            STRIDE
-        )
-        vertexArray.setVertexAttribPointer(
-            POSITION_COMPONENT_COUNT,
-            getTextureCoordinatesAttributeLocation(),
-            TEXTURE_COORDINATES_COMPONENT_COUNT,
-            STRIDE
-        )
-
-        //Draw right away for the camera positioning
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
-
-
-
-        // Don't draw the segmentation mask if it hasn't been updated yet.
-        if (byteBuffer == null) {
-            return
+                if (backgroundLikelihood > 0.9) {
+                    color = Color.argb(128, 255, 0, 255)
+                } else if (backgroundLikelihood > 0.2) {
+                    val alpha = (182.9 * backgroundLikelihood - 36.6 + 0.5).toInt()
+                    color = Color.argb(alpha, 255, 0, 255)
+                } else {
+                    color = Color.TRANSPARENT
+                }
+                buffer.putInt(color)
+            }
+            buffer.flip()
+            updateMaskData(buffer, maskWidth, maskHeight)
         }
-        // synchronized(lock) {
-        if (maskTextureId == -1) {
-            maskTextureId =
-                TextureHelper.createTextureFromColors(
-                    byteBuffer!!,
-                    byteBufferWidth,
-                    byteBufferHeight
-                )
-        }
-
-        // TODO update the matrix before drawing the mask
-//             // ... previous code ...
-//
-// // After drawing the first texture
-//             GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
-//
-// // Update the modelMatrix for the second texture
-// // ... your logic to update modelMatrix ...
-//
-// // Pass the updated matrix into the shader program.
-//             GLES20.glUniformMatrix4fv(getuMatrixLocation(), 1, false, modelMatrix, 0)
-//
-// // Bind your second texture and set it up
-// // ... your code to bind and set up second texture ...
-//
-// // Draw the second texture
-//             GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
-//
-//             GLES20.glDisable(GLES20.GL_BLEND);
-
-// Set the active texture unit to texture unit 1 for the segmentation mask.
-        byteBuffer!!.flip()
-        GLES20.glActiveTexture(GLES20.GL_TEXTURE1)
-        GLES20.glBindTexture(
-            GLES20.GL_TEXTURE_2D,
-            maskTextureId
-        )
-
-        GLES20.glTexSubImage2D(
-            GLES20.GL_TEXTURE_2D, 0, 0, 0,
-            byteBufferWidth, byteBufferHeight, GLES20.GL_RGBA,
-            GLES20.GL_UNSIGNED_BYTE, byteBuffer!!
-        )
-        // }
-        GLES20.glUniform1i(getuMaskTextureUnitLocation(), 1)
-
-        // bind the triangle + texture data
-        vertexArray.setVertexAttribPointer(
-            0,
-            getPositionAttributeLocation(),
-            POSITION_COMPONENT_COUNT,
-            STRIDE
-        )
-        vertexArray.setVertexAttribPointer(
-            POSITION_COMPONENT_COUNT,
-            getTextureCoordinatesAttributeLocation(),
-            TEXTURE_COORDINATES_COMPONENT_COUNT,
-            STRIDE
-        )
-
-        // lastly, draw the vertices...
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
-
-        GLES20.glDisable(GLES20.GL_BLEND);
-    }
-
-    private fun positionCameraCorrectly() {
-        Matrix.setIdentityM(modelMatrix, 0)
-        // As far as I can tell, the camera preview always comes in sideways.
-        // Every app who manages their own surface texture has to rotate it and set the scale.
-
-        val cameraAspectRatio = 1080f / 1920f   // 0.5625
-        val viewportAspectRatio = viewPortWidth.toFloat() / viewPortHeight.toFloat()
-        if (viewportAspectRatio > cameraAspectRatio) {
-            // The viewport is wider than the camera feed.
-            // We scale up in the Y direction to fill and crop.
-            val scale = viewportAspectRatio / cameraAspectRatio
-            Matrix.scaleM(modelMatrix, 0, 1f, scale, 1f)
-        } else {
-            // The viewport is taller than the camera feed.
-            // We scale up in the X direction to fill and crop.
-            val scale = cameraAspectRatio / viewportAspectRatio
-            Matrix.scaleM(modelMatrix, 0, scale, 1f, 1f)
-        }
-        Matrix.rotateM(modelMatrix, 0, 90f, 0f, 0f, 1f)
-        val mirrorMatrix = floatArrayOf(
-            -1f, 0f, 0f, 0f,
-            0f, 1f, 0f, 0f,
-            0f, 0f, 1f, 0f,
-            0f, 0f, 0f, 1f
-        )
-
-        Matrix.multiplyMM(modelMatrix, 0, mirrorMatrix, 0, modelMatrix, 0)
-
-        // TODO use this to scale and move the camera !!!
-        Matrix.scaleM(modelMatrix, 0, .5f, .5f, .5f)
-    }
-
-    companion object {
-        private const val POSITION_COMPONENT_COUNT = 2
-        private const val TEXTURE_COORDINATES_COMPONENT_COUNT = 2
-        private val STRIDE: Int = (POSITION_COMPONENT_COUNT
-            + TEXTURE_COORDINATES_COMPONENT_COUNT) * AirHockeyRenderer.BYTES_PER_FLOAT
-        private val VERTEX_DATA = floatArrayOf(
-            // Order of coordinates: X, Y, S, T
-            -1f, -1f, 0f, 1f,     // Bottom-left
-            1f, -1f, 1f, 1f,      // Bottom-right
-            1f, 1f, 1f, 0f,       // Top-right
-            -1f, 1f, 0f, 0f,      // Top-left
-            -1f, -1f, 0f, 1f      // Bottom-left again to close the fan
-        )
     }
 }
--- app/src/main/res/raw/camera_segmentation_fragment_shader.glsl
+++ app/src/main/res/raw/camera_segmentation_fragment_shader.glsl
@@ -5,19 +5,20 @@ precision mediump float;
 varying vec2 v_TextureCoordinates;
 
 uniform samplerExternalOES u_TextureUnit;
-uniform sampler2D u_MaskTexture;  // The RGBA buffer texture
+uniform sampler2D u_MaskTexture;
 
 void main() {
-    // Sample from the camera feed texture
-    vec4 cameraColor = texture2D(u_TextureUnit, v_TextureCoordinates);
+    // Rotate and mirror the texture coordinates for the camera texture
+    vec2 rotatedAndMirroredCoords = vec2(1.0 - v_TextureCoordinates.y, v_TextureCoordinates.x);
+    vec4 cameraColor = texture2D(u_TextureUnit, rotatedAndMirroredCoords);
 
-    // Sample the mask color
+    // Sample the mask color normally, as it's correctly oriented
     vec4 maskColor = texture2D(u_MaskTexture, v_TextureCoordinates);
 
     // Use the alpha channel of the mask to blend
     float blendFactor = maskColor.a;
 
-    // Simple alpha blending (assuming mask color RGB is the color you want to blend with the camera feed)
+    // Simple alpha blending
     vec4 blendedColor = mix(cameraColor, maskColor, blendFactor);
 
     gl_FragColor = blendedColor;
